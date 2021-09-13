6bdab38bb76a8e08f56ea78abf21bab06de9675a
Tobias Becker-Olsson <tbecker@MacBook-Air.tsbo.me>
2021-09-13 12:30:55 +0200

    Fullstack part1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part}</p>
+  )
+}
+
+const TotalExercises = (props) => {
+  const TotalExercises = props.exercises.reduce(
+    (previousScore, currentScore, index) => previousScore + currentScore, 
+    0);
+  return (
+    <p>{TotalExercises}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <TotalExercises exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+export default App
